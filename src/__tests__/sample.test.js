c072d90268c8fdfa71aeb757be1f8dc6304c8b20
Rahul Verma <rahul.v@sprinklr.com>
2024-08-28 15:19:39 +0530

    update sample test

--- src/__tests__/sample.test.js
+++ src/__tests__/sample.test.js
@@ -24,3 +24,40 @@ test('should add item to cart on item click', async () => {
 
   screen.getByTestId('cart-item-1');
 });
+
+test('Should remove item from cart', async () => {
+  render(<App />);
+
+  const itemToSelect = screen.getByTestId('item-1');
+
+  fireEvent.click(itemToSelect);
+
+  screen.getByTestId('cart-item-1');
+
+  const removeButton = screen.getByTestId('remove-item-1');
+
+  fireEvent.click(removeButton);
+
+  const nonExistingElement = screen.queryByTestId('remove-item-1');
+  expect(nonExistingElement).not.toBeInTheDocument();
+});
+
+test('Should remove item from cart if item is selected again from select list', async () => {
+  render(<App />);
+
+  const itemToSelect1 = screen.getByTestId('item-1');
+  const itemToSelect2 = screen.getByTestId('item-2');
+
+  fireEvent.click(itemToSelect1);
+  fireEvent.click(itemToSelect2);
+
+  screen.getByTestId('cart-item-1');
+  screen.getByTestId('cart-item-2');
+
+  fireEvent.click(itemToSelect2);
+
+  screen.getByTestId('cart-item-1');
+
+  const nonExistingElement = screen.queryByTestId('cart-item-2');
+  expect(nonExistingElement).not.toBeInTheDocument();
+});
